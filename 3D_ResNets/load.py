b16e1b5d0e7db9d575fb2394b3cbb96bade763df
russelrk <49132622+russelrk@users.noreply.github.com>
2023-12-30 13:48:51 -0500

    Update load.py
    corrected some typo

--- 3D_ResNets/load.py
+++ 3D_ResNets/load.py
@@ -1,11 +1,11 @@
 import os
 
-from 2D_to_3D_Weight_Transfer import resnet183D, resnet343D, resnet503D, resnet1013D, resnet1523D
+from 2D_to_3D_Weight_Transfer import resnet18_3D, resnet34_3D, resnet50_3D, resnet101_3D, resnet152_3D
 
 # how to import 3D resnet 18
-model = resnet183D(pre_trained = True, num_classes = 2)
+model = resnet18_3D(pre_trained = True, num_classes = 2)
 
 # how to import 3D resnet 34
-model = resnet343D(pre_trained = True, num_classes = 2)
+model = resnet34_3D(pre_trained = True, num_classes = 2)
 
 
