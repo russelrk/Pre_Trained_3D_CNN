f0fd24b9dd7c27b5f5d498b1d662b09c9ff2b611
russelrk <49132622+russelrk@users.noreply.github.com>
2023-12-30 13:50:53 -0500

    Update load.py

--- 3D_ResNets/load.py
+++ 3D_ResNets/load.py
@@ -1,6 +1,6 @@
 import os
 
-from 2D_to_3D_Weight_Transfer import resnet18_3D, resnet34_3D, resnet50_3D, resnet101_3D, resnet152_3D
+from ResNet3D_Weight_Transfer_2D_to_3D import resnet18_3D, resnet34_3D, resnet50_3D, resnet101_3D, resnet152_3D
 
 # how to import 3D resnet 18
 model = resnet18_3D(pre_trained = True, num_classes = 2)
