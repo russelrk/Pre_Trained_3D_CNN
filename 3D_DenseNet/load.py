25daf0d150e6bf7968b8d4f6d44bd699f69db26e
russelrk <49132622+russelrk@users.noreply.github.com>
2024-01-18 13:54:00 -0500

    Update load.py

--- 3D_DenseNet/load.py
+++ 3D_DenseNet/load.py
@@ -1,5 +1,5 @@
 from DenseNet3D_Weight_Transfer_2D_to_3D import densenet121_3d
 
-# how to import 3D resnet 18
+# how to import 3D densenet121
 model = densenet121_3d(pre_trained = True, num_classes = 2)
 
